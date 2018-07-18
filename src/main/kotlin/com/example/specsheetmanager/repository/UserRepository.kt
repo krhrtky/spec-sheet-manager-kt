4b36d1b3537d543dfe6111eb478a0d3fe023735b
takuya.kurihara <takuya.kurihara@cacco.co.jp>
2018-07-18 13:53:33 +0900

    [create] user登録までの処理を追加DBアクセス/JPAとの連携はまだ。

---
+++ src/main/kotlin/com/example/specsheetmanager/repository/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.example.specsheetmanager.repository
+
+import com.example.specsheetmanager.domain.User
+import org.springframework.stereotype.Repository
+
+@Repository
+class UserRepository() {
+
+    fun create(user: User) {
+        // TODO: insert処理を記述。 失敗はException
+    }
+}
