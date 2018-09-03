a0298e68c75af137160f31a6407a228b133ddf42
t-kurihara <takuya.kurihara00@gmail.com>
2018-09-03 23:52:05 +0900

    [change] card design.

--- src/main/javascript/src/components/Login.vue
+++ src/main/javascript/src/components/Login.vue
@@ -1,23 +1,31 @@
 <template>
-    <v-container>
-        <v-layout column wrap>
-            <v-flex>
-                <v-form>
-                    <v-text-field
-                            label="E-mail"
-                            required
-                    />
-                    <v-text-field
-                            label="Password"
-                            :type="'password'"
-                            required
-                    />
-                    <v-btn color="info">Login</v-btn>
-                    <v-btn color="warning">Clear</v-btn>
-                </v-form>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <v-card>
+        <v-card-title primary-title>
+            Login
+        </v-card-title>
+        <v-container
+                fluid
+                grid-list-lg
+        >
+            <v-layout column wrap>
+                <v-flex>
+                        <v-form>
+                            <v-text-field
+                                    label="E-mail"
+                                    required
+                            />
+                            <v-text-field
+                                    label="Password"
+                                    :type="'password'"
+                                    required
+                            />
+                            <v-btn color="info">Login</v-btn>
+                            <v-btn color="warning">Clear</v-btn>
+                        </v-form>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-card>
 </template>
 
 <script lang="ts">
@@ -30,5 +38,12 @@ export default class Login extends Vue {}
 </script>
 
 <style scoped>
+    .v-card {
+        margin: 10rem;
+    }
+
+    .flex > .v-card {
+        margin: 0;
+    }
 
 </style>
